56819c9b41eb0401ef551235799513b7bce62664
Franklin ospino <ospi89@hotmail.com>
2016-01-28 16:24:27 -0500

    habilitada carga desde servidor

---
+++ js/handsontable/Iniciartabla.js
@@ -0,0 +1,69 @@
+﻿var hot;
+
+angular.module("miApp", []).controller("tabla", function ($scope,$http) {
+        $http.get('/api/empleado/').then(function (d) {
+            var hotElement = document.getElementById('example');
+            hot = new Handsontable(hotElement, {
+                data: d.data, // see the Data tab
+                columns: [
+                    {
+                        data: "idempleado",
+                        type: "text",
+                    },
+                    {
+                        data: "cedula",
+                        type: "text",
+                    },
+                    {
+                        data: "nombre",
+                        type: "text"
+                    },
+                    {
+                        data: "tipo",
+                        type: "text"
+                    },
+                    {
+                        data: "departament",
+                        type: "text"
+                    },
+                    {
+                        data: "area",
+                        type: "text"
+                    },
+                    {
+                        data: "subarea",
+                        type: "text"
+                    },
+                    {
+                        data: "crewcd",
+                        type: "text"
+                    },
+                    {
+                        data: "rosterposition",
+                        type: "text"
+                    },
+                    {
+                        data: "unit",
+                        type: "text"
+                    }
+                ],
+                stretchH: "all",
+                rowHeaders: true,
+                columnSorting: true,
+                colHeaders: [
+                    "ID",
+                    "Cedula",
+                    "Nombre",
+                    "Type",
+                    "Departament",
+                    "Area",
+                    "SubArea",
+                    "Crew Cd",
+                    "Roster position",
+                    "Unit"
+                ],
+            });
+        });
+    
+    
+});
